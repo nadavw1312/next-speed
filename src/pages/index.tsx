b3e3c2cb7b780a50937c34678d4f23f09791cebe
nadavw1312 <nadavw1312@gmail.com>
2023-09-20 18:18:27 +0300

    - test

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,7 +2,7 @@ import Image from "next/image";
 import { Inter } from "next/font/google";
 import dynamic from "next/dynamic";
 
-const ParticlesContainerBrain = dynamic(() => import("../components/ParticlesContainerBrain"), { ssr: false });
+const ParticlesContainerBrain = dynamic(() => import("../components/ParticlesContainerBrain"));
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -10,6 +10,9 @@ export default function Home() {
   return (
     <main className={`min-h-screen flex flex-col justify-center relative ${inter.className}`}>
       <ParticlesContainerBrain id="tsparticles" />
+      <div>
+        <h1>Testing</h1>
+      </div>
     </main>
   );
 }
