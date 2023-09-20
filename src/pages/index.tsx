46a9d1ac63bb6130e86263a02ecbe2f0285e7365
nadavw1312 <nadavw1312@gmail.com>
2023-09-20 18:49:28 +0300

    - lazy loading

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,15 +1,24 @@
-import Image from "next/image";
 import { Inter } from "next/font/google";
 import dynamic from "next/dynamic";
+import { useEffect, useState } from "react";
 
 const ParticlesContainerBrain = dynamic(() => import("../components/ParticlesContainerBrain"));
 
 const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
+  const [particlesVisible, setParticlesVisible] = useState(false);
+
+  useEffect(() => {
+    // Simulate a delay (you can replace this with actual logic)
+    setTimeout(() => {
+      setParticlesVisible(true);
+    }, 1000); // Delay in milliseconds
+  }, []); // The empty dependency array ensures this effect runs once after initial render
+
   return (
     <main className={`min-h-screen flex flex-col justify-center relative ${inter.className}`}>
-      <ParticlesContainerBrain id="tsparticles" />
+      {particlesVisible && <ParticlesContainerBrain id="tsparticles" />}
       <div>
         <h1>Testing</h1>
       </div>
